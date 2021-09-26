16e0a7f82ecfe1a26415cf5e816d7ed4fb044ae4
Harsh080802 <90301323+Harsh080802@users.noreply.github.com>
2021-09-26 16:55:53 +0530

    Updated Exercise03_17
    using switch case and for loop

--- src/Exercise03/Exercise03_17.java
+++ src/Exercise03/Exercise03_17.java
@@ -1,8 +1,16 @@
 package Exercise03;
+/*Authar@Harsh Patel (20CS050)
+*(Game: scissor, rock, paper) Write a program that plays the popular scissor-rockpaper game. (A scissor can cut a paper, a rock can knock a scissor, and a paper can
+*wrap a rock.) The program randomly generates a number 0, 1, or 2 representing
+*scissor, rock, and paper. The program prompts the user to enter a number 0, 1, or
+*2 and displays a message indicating whether the user or the computer wins, loses,
+*or draws. Here are sample runs:
+*/
 import java.util.Random;
 import java.util.Scanner;
 public class Exercise03_17 {
         public static void main(String[] args) {
+           //Creating object
             Scanner sc= new Scanner(System.in);
             Random r = new Random();
             System.out.println("Enter 0 for rock \n Enter 1 for paper \n Enter 2 for scissors ");
@@ -12,6 +20,7 @@ public class Exercise03_17 {
             for(i=0;i<=5;i++) {
                 System.out.println("It's your turn");
                 int yt = sc.nextInt();
+                // using enhance switch case
                 switch (yt) {
                     case 0 -> System.out.println("You selected rock");
                     case 1 -> System.out.println("You selected paper");
